5d5a88dfcb1c3174b66cf84d8bb7170108ae19cd
Asad Orakzai <asad.orakzai@insaf.pk>
2024-01-06 11:29:43 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function App() {
+  let [color, setColor] = useState("white");
+
+  // const colorChange = () =>{
+  //     setColor("red")            // instead of this we have used call back function
+  // }
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+
+      {/*Reset Button*/}
+      <div className="absolute bottom-0 w-full flex justify-center mb-2">
+        <button
+          onClick={() => setColor("white")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "grey" }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
